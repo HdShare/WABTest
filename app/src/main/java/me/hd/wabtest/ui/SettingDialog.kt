74ca52c94b2a1a8fb066227beee663c50ba16861
hd <hdshare@vip.qq.com>
2026-08-23 19:21:33 +0800

    优化: 为设置弹窗增加搜索框与功能描述

--- app/src/main/java/me/hd/wabtest/ui/SettingDialog.kt
+++ app/src/main/java/me/hd/wabtest/ui/SettingDialog.kt
@@ -8,10 +8,16 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.StateListDrawable
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.BaseAdapter
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.LinearLayout.LayoutParams
 import android.widget.LinearLayout.LayoutParams.MATCH_PARENT
@@ -73,6 +79,7 @@ class SettingDialog(ctx: Activity) : AlertDialog.Builder(ctx) {
         val surfaceContainerColor = Color.parseColor(if (darkMode) "#1b2122" else "#e9eff0")
         val onSurfaceColor = Color.parseColor(if (darkMode) "#dee3e5" else "#171d1e")
         val onSurfaceVariantColor = Color.parseColor(if (darkMode) "#bfc8ca" else "#3f484a")
+        val outlineColor = Color.parseColor(if (darkMode) "#899294" else "#6f797a")
         val outlineVariantColor = Color.parseColor(if (darkMode) "#3f484a" else "#bfc8ca")
 
         fun showPopupMenu(
@@ -122,14 +129,22 @@ class SettingDialog(ctx: Activity) : AlertDialog.Builder(ctx) {
                     addState(intArrayOf(android.R.attr.state_pressed), ColorDrawable(outlineVariantColor))
                     addState(intArrayOf(), ColorDrawable(surfaceColor))
                 }
-                addView(TextView(ctx).apply {
-                    layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
-                        weight = 1f
+                addView(LinearLayout(ctx).apply {
+                    layoutParams = LayoutParams(0, WRAP_CONTENT, 1f).apply {
                         gravity = Gravity.START or Gravity.CENTER_VERTICAL
                     }
-                    tag = "title"
-                    textSize = 13f
-                    setTextColor(onSurfaceColor)
+                    orientation = VERTICAL
+                    addView(TextView(ctx).apply {
+                        tag = "title"
+                        textSize = 13f
+                        setTextColor(onSurfaceColor)
+                    })
+                    addView(TextView(ctx).apply {
+                        tag = "desc"
+                        textSize = 12f
+                        visibility = View.GONE
+                        setTextColor(outlineColor)
+                    })
                 })
                 addView(TextView(ctx).apply {
                     layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
@@ -152,69 +167,130 @@ class SettingDialog(ctx: Activity) : AlertDialog.Builder(ctx) {
             val config: ConfigItem? = null,
         )
 
-        val listItems = mutableListOf<ListItem>().apply {
-            TestManager.getList().forEach { testItem ->
-                if (testItem.configs.isNotEmpty()) {
-                    add(ListItem(group = testItem.group))
-                    testItem.configs.forEach { configItem ->
-                        add(ListItem(config = configItem))
+        val testItems = TestManager.getList()
+        val listItems = mutableListOf<ListItem>()
+
+        fun updateListItems(query: CharSequence?) {
+            val keyword = query?.toString()?.trim().orEmpty()
+            listItems.clear()
+            testItems.forEach { testItem ->
+                val configs = if (keyword.isEmpty() || testItem.group.contains(keyword)) {
+                    testItem.configs
+                } else {
+                    testItem.configs.filter { config ->
+                        config.key.contains(keyword) || config.title.contains(keyword) || config.desc.contains(keyword)
+                    }
+                }
+                if (configs.isNotEmpty()) {
+                    listItems.add(ListItem(group = testItem.group + "(${configs.size})"))
+                    configs.forEach { config ->
+                        listItems.add(ListItem(config = config))
                     }
                 }
             }
         }
 
+        val listAdapter = object : BaseAdapter() {
+            override fun getCount() = listItems.size
+            override fun getItem(position: Int) = listItems[position]
+            override fun getItemId(position: Int) = position.toLong()
+            override fun getViewTypeCount() = 2
+            override fun getItemViewType(position: Int): Int {
+                return if (getItem(position).group != null) 0 else 1
+            }
+
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val item = getItem(position)
+                return when (getItemViewType(position)) {
+                    0 -> {
+                        val group = requireNotNull(item.group)
+                        (convertView as? LinearLayout ?: getGroupView()).also { view ->
+                            val tvGroup = view.findViewWithTag("group") as? TextView
+                            tvGroup?.text = group
+                        }
+                    }
+
+                    1 -> {
+                        val config = requireNotNull(item.config)
+                        (convertView as? LinearLayout ?: getConfigView()).also { view ->
+                            val tvTitle = view.findViewWithTag("title") as? TextView
+                            tvTitle?.text = config.title
+                            val tvDesc = view.findViewWithTag("desc") as? TextView
+                            if (config.desc.isEmpty()) {
+                                tvDesc?.visibility = View.GONE
+                            } else {
+                                tvDesc?.visibility = View.VISIBLE
+                                tvDesc?.text = config.desc
+                            }
+                            val tvLabel = view.findViewWithTag("label") as? TextView
+                            tvLabel?.text = TestManager.getOptionsAlias(config.options, config.key)
+                            view.setOnClickListener { row ->
+                                val alias = TestManager.getOptionsAlias(config.options, config.key)
+                                showPopupMenu(row, config.options, alias) { selectedOption ->
+                                    ExptManager.putArgValue(config.key, selectedOption.value)
+                                    tvLabel?.text = selectedOption.alias
+                                }
+                            }
+                        }
+                    }
+
+                    else -> error("未知的列表项类型")
+                }
+            }
+        }
+        updateListItems(null)
+
         return LinearLayout(ctx).apply {
             layoutParams = LayoutParams(MATCH_PARENT, MATCH_PARENT)
             orientation = VERTICAL
             setBackgroundColor(surfaceContainerColor)
-            // TODO 搜索框预留位
+
+            addView(LinearLayout(ctx).apply {
+                layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
+                    setMargins(dp12, dp12, dp12, dp12)
+                }
+                setBackgroundColor(surfaceColor)
+
+                val editView = EditText(ctx).apply {
+                    layoutParams = LayoutParams(0, MATCH_PARENT, 1f)
+                    imeOptions = EditorInfo.IME_ACTION_SEARCH
+                    inputType = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+                    background = null
+                    textSize = 13f
+                    hint = "搜索..."
+                    addTextChangedListener(object : TextWatcher {
+                        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                        override fun afterTextChanged(s: Editable?) {}
+                        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                            updateListItems(s)
+                            listAdapter.notifyDataSetChanged()
+                        }
+                    })
+                }
+                addView(editView)
+
+                addView(ImageView(ctx).apply {
+                    layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+                    setPadding(dp4, dp4, dp4, dp4)
+                    background = StateListDrawable().apply {
+                        addState(intArrayOf(android.R.attr.state_pressed), ColorDrawable(outlineVariantColor))
+                        addState(intArrayOf(), ColorDrawable(Color.TRANSPARENT))
+                    }
+                    isClickable = true
+                    setImageResource(android.R.drawable.ic_menu_close_clear_cancel)
+                    setOnClickListener {
+                        editView.setText("")
+                    }
+                })
+            })
+
             addView(ListView(ctx).apply {
                 layoutParams = LayoutParams(MATCH_PARENT, 0, 1f)
-                setPadding(dp12, dp12, dp12, dp12)
+                setPadding(dp12, 0, dp12, dp12)
                 selector = ColorDrawable(Color.TRANSPARENT)
                 divider = ColorDrawable(outlineVariantColor)
                 dividerHeight = ctx.dp2px(0.5f)
-                adapter = object : BaseAdapter() {
-                    override fun getCount() = listItems.size
-                    override fun getItem(position: Int) = listItems[position]
-                    override fun getItemId(position: Int) = position.toLong()
-                    override fun getViewTypeCount() = 2
-                    override fun getItemViewType(position: Int): Int {
-                        return if (getItem(position).group != null) 0 else 1
-                    }
-
-                    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-                        val item = getItem(position)
-                        return when (getItemViewType(position)) {
-                            0 -> {
-                                val group = requireNotNull(item.group)
-                                (convertView as? LinearLayout ?: getGroupView()).also { view ->
-                                    val tvGroup = view.findViewWithTag("group") as? TextView
-                                    tvGroup?.text = group
-                                }
-                            }
-
-                            1 -> {
-                                val config = requireNotNull(item.config)
-                                (convertView as? LinearLayout ?: getConfigView()).also { view ->
-                                    val tvTitle = view.findViewWithTag("title") as? TextView
-                                    tvTitle?.text = config.title
-                                    val tvLabel = view.findViewWithTag("label") as? TextView
-                                    tvLabel?.text = TestManager.getOptionsAlias(config.options, config.key)
-                                    view.setOnClickListener { row ->
-                                        val alias = TestManager.getOptionsAlias(config.options, config.key)
-                                        showPopupMenu(row, config.options, alias) { selectedOption ->
-                                            ExptManager.putArgValue(config.key, selectedOption.value)
-                                            tvLabel?.text = selectedOption.alias
-                                        }
-                                    }
-                                }
-                            }
-
-                            else -> error("未知的列表项类型")
-                        }
-                    }
-                }
+                adapter = listAdapter
             })
         }
     }
